c4831104d890818a2e3f962df32d0b57ad79e40f
Parvez Kose <flamboyanz@gmail.com>
2019-08-04 21:32:09 -0400

    reducer index

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import courses from "./courseReducer";
+
+const rootReducer = combineReducers({
+  //courses: courses
+  courses
+});
+
+export default rootReducer;
